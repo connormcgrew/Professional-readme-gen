86c9b1960bac68d0abe1031befe2b1a333d72f73
Connor McGrew <connormcgrew003@gmail.com>
2023-04-21 11:15:28 -0600

    inital commit and questions

---
+++ Develop/index.js
@@ -0,0 +1,87 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: nameInput => !!nameInput || 'Please enter the title of your project!'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your project. (Required)',
+        validate: descriptionInput => !!descriptionInput || 'Please enter a description of your project!'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter installation instructions for your project. (Required)',
+        validate: installationInput => !!installationInput || 'Please enter installation instructions for your project!'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter usage information for your project. (Required)',
+        validate: usageInput => !!usageInput || 'Please enter usage information for your project!'
+    },
+    {
+        type: 'input',
+        name: 'confirm',
+        message: 'Would you like to include a license for your project? (Required)',
+        validate: confirmInput => !!confirmInput || 'Please enter yes or no!'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        when: answers => answers.confirmLicense,
+        message: 'Please enter license information for your project. (Required)',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'licenseName',
+        message: 'Please enter the name of the license you would like to use. (Required)',
+        when: answers => answers.confirmLicense && answers.license !== 'None',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please enter contribution guidelines for your project. (Required)',
+        validate: contributingInput => !!contributingInput || 'Please enter contribution guidelines for your project!'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter test instructions for your project. (Required)',
+        validate: testsInput => !!testsInput || 'Please enter test instructions for your project!'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Please enter your contact information for quesitons and support!',
+    },
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) throw err;
+        console.log('README created!');
+    });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+        writeToFile('./dist/README.md', generateMarkdown(data));
+    });
+}
+
+// Function call to initialize app
+init();
